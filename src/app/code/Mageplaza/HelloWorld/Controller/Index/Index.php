dfe7df8e9af63d4d76ac0489f26425c71d2ca6f2
Taufiq Ismail <taufiqismail04@gmail.com>
2021-09-13 21:28:57 +0700

    Section 02 - Create Model (CRUD)

--- src/app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
+++ src/app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -2,23 +2,32 @@
 
 namespace Mageplaza\HelloWorld\Controller\Index;
 
-use \Magento\Framework\App\Action\Context;
-use \Magento\Framework\View\Result\PageFactory;
-
 class Index extends \Magento\Framework\App\Action\Action
 {
 	protected $_pageFactory;
 
+	protected $_postFactory;
+
 	public function __construct(
-		Context $context,
-		PageFactory $pageFactory
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+		\Mageplaza\HelloWorld\Model\PostFactory $postFactory
 	) {
 		$this->_pageFactory = $pageFactory;
+		$this->_postFactory = $postFactory;
 		return parent::__construct($context);
 	}
 
 	public function execute()
 	{
+		$post = $this->_postFactory->create();
+		$collection = $post->getCollection();
+		foreach ($collection as $item) {
+			echo "<pre>";
+			print_r($item->getData());
+			echo "</pre>";
+		}
+		exit();
 		return $this->_pageFactory->create();
 	}
 }
